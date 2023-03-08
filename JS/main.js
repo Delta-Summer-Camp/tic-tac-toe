5c0a45871c804a42f23b37ee9668c18dfdc67171
Dmitry Ablov <d.ablov@gmail.com>
2023-03-08 11:11:12 +0100

    3 марта разбирались, как работает функция onclick для кнопок, и осваивали свойство .innerText html-объектов. Также освоили невидимый тег <span>. В папке 2023-03-03 - то, что мы сделали до этого дня.

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,18 @@
-function functionHello() {
-	alert("Hello!")
+/*
+let count = 0;
+
+function addOne() {
+	count = count + 1;
+	const countNode = document.getElementById("counter");
+	countNode.innerText = count;
 }
-function functionGoodbye() {
-	alert("Good bye!")
+
+function subOne() {
+	document.getElementById("counter").innerText = --count;
+}
+*/
+
+// В cellNode находится то же самое, что мы получаем из функции document.getElementBuId
+function imClicked(cellNode) {
+	
 }
