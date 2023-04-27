a0c7bae0954a38901e47e2f18f995e576a94760f
Dmitry Ablov <d.ablov@gmail.com>
2023-04-27 10:03:59 +0200

    Материалы занятия 21 апреля. Продолжили программу "Ну погоди!". Теперь если волк ловит яйцо, срабатывает счётчик и сбрасывается новое яйцо.

--- JS/main.js
+++ JS/main.js
@@ -1,14 +1,15 @@
-var timer;
+const MAXCOL = 5;
+const MAXROW = 5;
 var wolfPosition = 3;
-var eggPosition = 1;
+var wins = 0;
+
 
 function init() {
-	timer = setInterval(ticker, 1000);
-
+	
 	document.getElementById("53").style.backgroundImage="url('IMG/wolf.png')";
-	document.getElementById("11").style.backgroundImage="url('IMG/egg.png')";
-
 	addEventListener("keydown", keyDown);
+
+	newEgg();
 }
 
 function ticker() {
@@ -48,4 +49,48 @@ function keyDown(event) {
 			document.getElementById(id).style.backgroundImage="url('IMG/wolf.png')";
 		} 
 	}
+}
+
+// Создаёт новое яйцо в случайном ряду и управляет им
+function newEgg() {
+	// Генерируем случайное число от 1 до MAXCOL
+	const col = Math.floor(Math.random() * MAXCOL) + 1;
+	var row = 1;
+	document.getElementById(row * 10 + col).style.backgroundImage="url('IMG/egg.png')";
+
+	const timer = setInterval(ticker, 1000);
+
+	function ticker() {
+		document.getElementById(row * 10 + col).style.backgroundImage = "none";
+
+		if (row < MAXROW - 1) { // Двигаем яйцо вниз
+			row = row + 1;
+			document.getElementById(row * 10 + col).style.backgroundImage="url('IMG/egg.png')";
+		} else { 
+			// "Отключаем" яйцо
+			clearInterval(timer);
+
+			// Решаем, словил ли яйцо волк
+			if (col == wolfPosition) { // Выиграл!
+				win(col);
+			} else {
+				lose(col);
+			}
+		}
+	}
+}
+
+function win(col) {
+	// Изображаем волка с яйцом
+	document.getElementById(MAXROW * 10 + col).style.backgroundImage="url('IMG/wolf_win.png')";
+
+	// Увеличиваем счётчик выигрышей
+	document.getElementById("counter").innerText = ++wins;
+
+	// "Бросаем" новое яйцо
+	newEgg();
+}
+
+function lose(col) {
+
 }
